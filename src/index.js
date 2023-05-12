1627c0ac78b1bf32b69d98beacc7091e4325c3da
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-05-12 17:02:36 +0300

    implement combineReducers

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
+
 import { createStore } from "redux";
-import reducer from "./features/books/booksSlice";
+import rootReducer from "./reducers";
 
 const store = createStore(
-  reducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
 
 ReactDOM.render(
   <Provider store={store}>
